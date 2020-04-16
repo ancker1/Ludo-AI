dfc2d77fba300ad3a03c70b2ad1cfd9d9d11d5e0
Emil Ancker <emil@ancker.com>
2020-04-16 23:11:41 +0200

    GA + NN works

--- GeneticAlgorithm/individual.py
+++ GeneticAlgorithm/individual.py
@@ -54,21 +54,18 @@ class GANNIndividual:
     """
     name = 'NN Individual'
 
-    input_size  = 4 * 59 + 1 # 4 tokens per player
-    output_size = 4     # only 4 actions
-    hidden_neurons = 100 # 20 hidden neurons
+    input_size  = 4 * 59 + 1 # 4 tokens per player & bias
+    output_size = 4          # only 4 actions
+    hidden_neurons = 100     # hidden neurons
     gene_count = hidden_neurons * input_size + hidden_neurons + hidden_neurons * output_size + output_size
 
     def __init__(self):
         self.hidden_neurons = 100
         self.input_size     = 4 * 59 + 1
-        self.output_size    = 4
+        self.output_size    = 1
 
         self.W1 = np.zeros((self.hidden_neurons, self.input_size))
-        self.b1 = np.zeros((self.hidden_neurons, 1))
-        self.W2 = np.zeros((self.output_size, self.hidden_neurons))
-        self.b2 = np.zeros((self.output_size, 1))
-            
+        self.W2 = np.zeros((self.output_size, self.hidden_neurons))          
     
 
     def load_chromosome(self, chromosome):
@@ -76,13 +73,10 @@ class GANNIndividual:
         Chromosome contains weights for NN
         """
         self.chromosome = chromosome
-        w1_siz = self.hidden_neurons*self.input_size
-        w2_siz = self.output_size*self.hidden_neurons
+        w1_siz = self.hidden_neurons * self.input_size
+        w2_siz = self.output_size * self.hidden_neurons
         self.W1 = self.chromosome[0 : w1_siz].reshape((self.hidden_neurons, self.input_size))
         self.W2 = self.chromosome[w1_siz : w1_siz + w2_siz].reshape((self.output_size, self.hidden_neurons))
-        self.b1 = self.chromosome[w1_siz + w2_siz : w1_siz + w2_siz + self.hidden_neurons].reshape((self.hidden_neurons, 1))
-        self.b2 = self.chromosome[w1_siz + w2_siz + self.hidden_neurons : w1_siz + w2_siz + self.hidden_neurons + self.output_size].reshape((self.output_size, 1))
-
 
     # tanh
     @staticmethod
@@ -108,23 +102,32 @@ class GANNIndividual:
                 repre[i, min(idx + 1, 58)] += 1
         return repre.ravel()
 
-    def forward(self, state, dice_roll):
-        temp_input = np.append(self.translate(state.state), dice_roll)
+    def forward(self, state):
+
+        temp_input = np.append(self.translate(state.state), 1)
         net_input = np.ravel(temp_input).reshape((self.input_size, 1))
-        activation = self.W1 @ net_input + self.b1
-        activation = activation * np.sqrt(1/self.input_size)
+
+        activation = self.W1 @ net_input
+        activation = activation * np.sqrt(1 / self.input_size)
         activation = self.tanh(activation)
 
-        output = self.W2 @ activation + self.b2
+        output = self.W2 @ activation
         #output = softmax(output)
         return output
         
 
     def evaluate_actions(self, state, next_states, dice_roll):
-        action_values = self.forward(state, dice_roll)
-        for i in range(4):
-            if next_states[i] is False:
+        action_values = np.zeros(4)
+        for i, next_state in enumerate(next_states):
+            if next_state is False:
                 action_values[i] = -100
+            else:
+                action_values[i] = self.forward(next_state)
+        #action_values = self.forward(state, dice_roll)
+        #for i in range(4):
+        #    if next_states[i] is False:
+        #        action_values[i] = -100
+        #print(action_values)
         return np.argmax(action_values)
 
     def play(self, state, dice_roll, next_states):
